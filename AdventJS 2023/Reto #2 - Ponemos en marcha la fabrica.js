75d8af9487071d47bff5a3fed0b41dc860eadbac
marcosd59 <damian.marcospool@gmail.com>
2024-01-16 19:53:56 -0500

    add solution for challenge #2

--- AdventJS 2023/Reto #2 - Ponemos en marcha la fabrica.js
+++ AdventJS 2023/Reto #2 - Ponemos en marcha la fabrica.js
@@ -1,8 +1,10 @@
 /*
-Reto #2: Ponemos en marcha la fabrica.
+Reto #2: 🏭 Ponemos en marcha la fábrica
 
 En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
+
 Los regalos son cadenas de texto y los materiales son caracteres. Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
+
 Un regalo se puede fabricar si contamos con todos los materiales necesarios para fabricarlo.
 */
 
