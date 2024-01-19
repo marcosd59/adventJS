c371b58c1d9a140bde99c83cdc52ffbd10e79102
marcosd59 <damian.marcospool@gmail.com>
2024-01-18 19:27:16 -0500

    Descripcion del reto #10

--- AdventJS 2023/Reto #10 - Crea tu propio árbol de navidad.js
+++ AdventJS 2023/Reto #10 - Crea tu propio árbol de navidad.js
@@ -8,13 +8,6 @@ Para crearlo nos pasan una cadena de caracteres para formar el árbol y un núme
 Cada carácter de la cadena representa un adorno del árbol, y vamos utilizándolos de forma cíclica hasta llegar a la altura indicada. Como mínimo siempre nos pasarán uno.
 
 Debemos devolver un string multilínea con el árbol de Navidad formado con los adornos, la altura indicada más una última línea con el tronco formado por el carácter | en el centro y, finalmente, un salto de línea \n.
-
-Nota:
-
-El árbol siempre debe estar centrado, para ello añade espacios en blanco a la izquierda de cada línea.
-Crea espacios sólo a la izquierda de cada línea del árbol. No dejes espacios en blanco a la derecha.
-Los adornos tienen un espacio en blanco entre ellos de separación.
-Si te fallan los tests y visualmente parece que el árbol está bien, comprueba que no haya espacios en blanco que sobren, especialmente a la derecha de cada línea.
 */
 
 function createChristmasTree(ornaments, height) {
@@ -37,4 +30,10 @@ Si recibimos la cadena *@o y el número 3, el árbol que debemos devolver es:
 * @ o
   |
 
+Nota:
+
+El árbol siempre debe estar centrado, para ello añade espacios en blanco a la izquierda de cada línea.
+Crea espacios sólo a la izquierda de cada línea del árbol. No dejes espacios en blanco a la derecha.
+Los adornos tienen un espacio en blanco entre ellos de separación.
+Si te fallan los tests y visualmente parece que el árbol está bien, comprueba que no haya espacios en blanco que sobren, especialmente a la derecha de cada línea.
 */
