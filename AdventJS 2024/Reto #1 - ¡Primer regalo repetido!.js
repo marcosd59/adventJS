2a3c1a347472c631d338de066965d050a8e277ab
Marcos Pool <damian.marcospool@gmail.com>
2024-12-02 09:52:04 -0500

    add solution for challenge #1

---
+++ AdventJS 2024/Reto #1 - ¡Primer regalo repetido!.js
@@ -0,0 +1,25 @@
+/*
+Reto #1: 🎁 ¡Primer regalo repetido!
+
+Santa Claus 🎅 ha recibido una lista de números mágicos que representan regalos 🎁, pero algunos de ellos están duplicados y deben ser eliminados para evitar confusiones. Además, los regalos deben ser ordenados en orden ascendente antes de entregárselos a los elfos.
+
+Tu tarea es escribir una función que reciba una lista de números enteros (que pueden incluir duplicados) y devuelva una nueva lista sin duplicados, ordenada en orden ascendente..
+*/
+
+function prepareGifts(gifts) {
+  let uniqueGifts = [...new Set(gifts)];
+  return uniqueGifts.sort((a, b) => a - b) || [];
+}
+
+const gifts1 = [3, 1, 2, 3, 4, 2, 5];
+const preparedGifts1 = prepareGifts(gifts1);
+console.log(preparedGifts1); // [1, 2, 3, 4, 5]
+
+const gifts2 = [6, 5, 5, 5, 5];
+const preparedGifts2 = prepareGifts(gifts2);
+console.log(preparedGifts2); // [5, 6]
+
+const gifts3 = [];
+const preparedGifts3 = prepareGifts(gifts3);
+console.log(preparedGifts3); // []
+// No hay regalos, la lista queda vacía
